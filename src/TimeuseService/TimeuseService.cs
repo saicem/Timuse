c4493c780043d7ee858abf656660ff7a630f4043
saicem <saicem@qq.com>
2023-05-28 20:24:53 +0800

    add project file

---
+++ src/TimeuseService/TimeuseService.cs
@@ -0,0 +1,105 @@
+﻿namespace TimeuseService;
+
+using System.Runtime.InteropServices;
+using System.Text;
+
+public static class TimeuseService
+{
+    [UnmanagedCallersOnly(EntryPoint = nameof(OnSwitch))]
+    public static void OnSwitch(nint lpName, nint lpPath)
+    {
+        var time = DateTime.UtcNow;
+        var name = Marshal.PtrToStringUni(lpName);
+        var path = Marshal.PtrToStringUni(lpPath);
+
+        if (name == null || path == null) return;
+
+        HandleStatistics();
+
+        recordingApplicationName = name;
+        recordingApplicationFocusTime = time;
+        recordingApplicationPath = path;
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = nameof(InitService))]
+    public static void InitService()
+    {
+        Console.WriteLine("Timuse service starting...");
+
+        if (!Directory.Exists(basePath)) Directory.CreateDirectory(basePath);
+
+        using var stream = new FileStream(pathProcessMap, FileMode.OpenOrCreate, FileAccess.Read);
+        using var reader = new BinaryReader(stream, Encoding.UTF8);
+
+        ushort id = 0;
+        // read exsisting application info
+        while (reader.BaseStream.Position < reader.BaseStream.Length)
+        {
+            id = reader.ReadUInt16();
+            var path = reader.ReadString();
+            var name = reader.ReadString();
+            idDictionary.Add(path, id);
+
+            Console.WriteLine($"application {id}: {name}, path={path}");
+        }
+        reader.Close();
+        currentMaxId = id;
+
+        var recordStream = new FileStream(pathRecord, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+        recordStream.Seek(recordStream.Length, SeekOrigin.Begin);
+        recordWriter = new BinaryWriter(recordStream, Encoding.UTF8);
+
+        Console.WriteLine("Timuse service started.");
+    }
+
+    private static string recordingApplicationName = "";
+    private static string recordingApplicationPath = "";
+    private static DateTime recordingApplicationFocusTime;
+
+    private static readonly DateTime timeStampOrigin = new (2000, 1, 1);
+    private static readonly Dictionary<string, ushort> idDictionary = new();
+    private static BinaryWriter? recordWriter;
+    private static ushort currentMaxId;
+
+    private static readonly string basePath = Path.Join(Path.GetDirectoryName(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)), "Local/Timuse");
+    private static readonly string pathProcessMap = Path.Join(basePath, "map.bin");
+    private static readonly string pathRecord = Path.Join(basePath, "record.bin");
+
+    private static void HandleStatistics()
+    {
+        if (recordWriter is null) return;
+        if (string.IsNullOrEmpty(recordingApplicationName)) return;
+
+        var timestamp = (int)(recordingApplicationFocusTime - timeStampOrigin).TotalSeconds;
+        var duration = (int)(DateTime.UtcNow - recordingApplicationFocusTime).TotalMilliseconds;
+
+        var success = idDictionary.TryGetValue(recordingApplicationPath, out ushort id);
+        if (!success)
+        {
+            currentMaxId++;
+            id = currentMaxId;
+            idDictionary.Add(recordingApplicationPath, id);
+            SaveApplicationInfo(recordingApplicationPath, recordingApplicationName, id);
+        }
+
+        recordWriter.Write(id);
+        recordWriter.Write(timestamp);
+        recordWriter.Write(duration);
+        recordWriter.Flush();
+        
+        Console.WriteLine($"{recordingApplicationName}({id}):{timestamp},{duration}");
+    }
+
+    private static void SaveApplicationInfo(string path, string name, ushort id)
+    {
+        using var stream = File.OpenWrite(pathProcessMap);
+        stream.Seek(stream.Length, SeekOrigin.Begin);
+        using var writer = new BinaryWriter(stream, Encoding.UTF8);
+        writer.Write(id);
+        writer.Write(path);
+        writer.Write(name);
+        writer.Flush();
+
+        Console.WriteLine($"new application: {name}({id}), path={path}");
+    }
+}
---
+++ src/TimuseService/TimuseService.cpp
@@ -0,0 +1,81 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+void LoadHook();
+void WinEventproc(HWINEVENTHOOK, DWORD, HWND, LONG, LONG, DWORD, DWORD);
+
+typedef void (*OnSwitchProc)(LPVOID lpName, LPVOID lpPath);
+
+OnSwitchProc onSwitch;
+FARPROC initService;
+
+WCHAR lpPathBuffer[MAX_PATH];
+WCHAR lpCmdBuffer[44];
+
+int main()
+{
+	HMODULE hLibService = LoadLibrary(L"TimeuseService.dll");
+	if (hLibService == NULL) return 1;
+
+	onSwitch = (OnSwitchProc)GetProcAddress(hLibService, "OnSwitch");
+	initService = GetProcAddress(hLibService, "InitService");
+
+	if (!initService || !onSwitch) return 1;
+
+	initService();
+	LoadHook();
+
+	MSG msg = { };
+
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
+
+void LoadHook()
+{
+	auto hModule = GetModuleHandle(nullptr);
+	SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, NULL, &WinEventproc, 0, 0, WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS);
+}
+
+void WinEventproc(HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd, LONG idObject, LONG idChild, DWORD idEventThread, DWORD dwmsEventTime)
+{
+	DWORD dwProcessId;
+	GetWindowThreadProcessId(hwnd, &dwProcessId);
+	auto hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, true, dwProcessId);
+
+	DWORD dwPathLength = MAX_PATH;
+	QueryFullProcessImageName(hProcess, 0, lpPathBuffer, &dwPathLength);
+
+	DWORD dwFileVersionInfoSize = GetFileVersionInfoSize(lpPathBuffer, nullptr);
+
+	BYTE* lpVersionInfoBuffer = new BYTE[dwFileVersionInfoSize];
+	GetFileVersionInfo(lpPathBuffer, NULL, dwFileVersionInfoSize, lpVersionInfoBuffer);
+
+	LPVOID lpbuffer;
+	UINT bufferLeng;
+	BOOL success = VerQueryValue(lpVersionInfoBuffer, L"\\VarFileInfo\\Translation", &lpbuffer, &bufferLeng);
+	
+	DWORD dwLang = 0x040904E4;
+	if (success) 
+	{
+		dwLang = *((LPDWORD)lpbuffer);
+		dwLang = ((dwLang & 0xffff) << 16) | (dwLang >> 16);
+	}
+
+	swprintf_s(lpCmdBuffer, L"\\\\StringFileInfo\\\\%08x\\\\FileDescription", dwLang);
+
+	success = VerQueryValue(lpVersionInfoBuffer, lpCmdBuffer, &lpbuffer, &bufferLeng);
+	if (!success)
+	{
+		delete[](lpVersionInfoBuffer);
+		return;
+	}
+
+	onSwitch(lpbuffer, lpPathBuffer);
+	delete[](lpVersionInfoBuffer);
+}
