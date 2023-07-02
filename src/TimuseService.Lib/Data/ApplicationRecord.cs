b414610bab521e1bb794de90e6844e5cb2dc887f
saicem <saicem@qq.com>
2023-07-02 22:44:29 +0800

    refactor: use ttick as the time unit for record

--- src/TimuseService.Lib/Data/ApplicationRecord.cs
+++ src/TimuseService.Lib/Data/ApplicationRecord.cs
@@ -5,6 +5,8 @@ using System.Runtime.InteropServices;
 [StructLayout(LayoutKind.Sequential)]
 public struct ApplicationRecord
 {
+    public const long TTick = TimeSpan.TicksPerMillisecond * 10;
+
     // 63.....................................39.........................15........0
     //  | timestamp(seconds from 00:00:00 UTC) | timespan(centi-seconds)  | app id |
     public ulong Data;
@@ -15,15 +17,37 @@ public struct ApplicationRecord
         set => Data = Data & 0xFFFFFF_FFFFFF_0000 | value;
     }
 
-    public int TimeSpan
+    public int DurationTTicks
     {
         get => (int)((Data & 0x000000_FFFFFF_0000) >> 16);
         set => Data = Data & 0xFFFFFF_000000_FFFF | ((ulong)value << 16);
     }
 
-    public int TimeStamp
+    public int RecordStartAtTTicks
     {
         get => (int)((Data & 0xFFFFFF_000000_0000) >> 40);
         set => Data = Data & 0x000000_FFFFFF_FFFF | ((ulong)value << 40);
+    }
+
+    public TimeSpan Duration
+    {
+        get => TTicksToTimeSpan(DurationTTicks);
+        set => DurationTTicks = TimeSpanToTTicks(value);
+    }
+
+    public TimeSpan RecordStartAt
+    {
+        get => TTicksToTimeSpan(RecordStartAtTTicks);
+        set => RecordStartAtTTicks = TimeSpanToTTicks(value);
+    }
+
+    public static TimeSpan TTicksToTimeSpan(int tticks)
+    {
+        return TimeSpan.FromTicks(TTick * tticks);
+    }
+
+    public static int TimeSpanToTTicks(TimeSpan timeSpan)
+    {
+        return (int)(timeSpan.Ticks / TTick);
     }
 }
