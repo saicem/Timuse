1b7a863faac8f96e23e07f6cd4491db100123bfa
AZhrZho <1137361788@qq.com>
2023-12-23 17:09:20 +0800

    feat: add winui project

---
+++ src/Teanuts/Teanuts/Extension/WindowExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Teanuts.Extension;
+
+internal static class WindowExtension
+{
+    public static bool TrySetMicaBackdrop(this Window window, bool useMicaAlt)
+    {
+        if (Microsoft.UI.Composition.SystemBackdrops.MicaController.IsSupported())
+        {
+            Microsoft.UI.Xaml.Media.MicaBackdrop micaBackdrop = new Microsoft.UI.Xaml.Media.MicaBackdrop();
+            micaBackdrop.Kind = useMicaAlt ? Microsoft.UI.Composition.SystemBackdrops.MicaKind.BaseAlt : Microsoft.UI.Composition.SystemBackdrops.MicaKind.Base;
+            window.SystemBackdrop = micaBackdrop;
+
+            return true; // Succeeded.
+        }
+
+        return false; // Mica is not supported on this system.
+    }
+
+    public static bool TrySetDesktopAcrylicBackdrop(this Window window)
+    {
+        if (Microsoft.UI.Composition.SystemBackdrops.DesktopAcrylicController.IsSupported())
+        {
+            Microsoft.UI.Xaml.Media.DesktopAcrylicBackdrop desktopAcrylicBackdrop = new Microsoft.UI.Xaml.Media.DesktopAcrylicBackdrop();
+            window.SystemBackdrop = desktopAcrylicBackdrop;
+
+            return true; // Succeeded.
+        }
+
+        return false; // DesktopAcrylic is not supported on this system.
+    }
+}
---
+++ src/Teanuts/Teanuts/Host/ApplicationHost.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI.Xaml;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using WinRT;
+
+namespace Teanuts.Host;
+
+internal static partial class ApplicationHost
+{
+    [LibraryImport("Microsoft.ui.xaml.dll")]
+    private static partial void XamlCheckProcessRequirements();
+
+    public static IHost Build<TApplication, TMainWindow>(string[] args, Action<IHostBuilder> configureHostBuilder)
+        where TApplication : Application
+        where TMainWindow : Window
+    {
+        var hostBuilder = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args);
+        hostBuilder.ConfigureServices((context, services) =>
+        {
+            services.AddSingleton<TApplication>();
+            services.AddSingleton<TMainWindow>();
+            services.AddHostedService<AppStartupService<TApplication>>();
+        });
+
+        configureHostBuilder?.Invoke(hostBuilder);
+
+        if (Thread.CurrentThread.GetApartmentState() != ApartmentState.STA)
+        {
+            Thread.CurrentThread.SetApartmentState(ApartmentState.Unknown);
+            Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
+        }
+
+        XamlCheckProcessRequirements();
+        ComWrappersSupport.InitializeComWrappers();
+
+        var host = hostBuilder.Build();
+        return host;
+    }
+}
